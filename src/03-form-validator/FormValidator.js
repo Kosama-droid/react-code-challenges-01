d7aa358a4d3954d02f748849116e77af11b7f6a0
Ko <55330447+Kosama-droid@users.noreply.github.com>
2023-03-20 19:36:39 +0000

    finished exercice with help

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -5,8 +5,36 @@ export default function FormValidator () {
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
 
+  const [message, setMessage] = useState("")
+
+  const findErrors = () => {
+    const errors = [];
+
+    if (!email || !password || !passwordConfirm){
+      errors.push("All filled must be filled in");
+    }
+    if([...email].filter(i => i === "@").length !== 1){
+      errors.push("An email must have exactly one @ sign")
+    }
+    if(password.length < 8){
+      errors.push("Length of password must be larger or equal to 8")
+    }
+    if(password !== passwordConfirm) {
+      errors.push("Passwords must match")
+    }
+
+    return errors
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const errors = findErrors()
+    setMessage(errors.length ? errors.join(", ") : "User created!")
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
@@ -23,6 +51,7 @@ export default function FormValidator () {
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
       />
+      {message}
       <input type='submit' value='Submit' />
     </form>
   )
